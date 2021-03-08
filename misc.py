347cac01a248a5f5e9d097407e42b0e8a001e5d7
KacperRuz <kacper.ruzczynski@tlen.pl>
2021-03-08 02:24:49 +0100

    text change

--- misc.py
+++ misc.py
@@ -10,13 +10,13 @@ def make_spaces(max_len, txt):
 
 async def print_commands(message):
     text = """```bash
-    'inv' lub 'ekw' (otwórz ekwipunek)
-    'uzyj' [przedmiot]
-    'sprawdz' [element otoczenia]
-    'zaloz' [przedmiot]
-    'atakuj' [cel]
-    'bron' (sprawdź jaką trzymasz broń)
-    'otoczenie' lub '!env' (ponownie sprawdź otoczenie)
-    ```"""
+'inv' lub 'ekw' (otwórz ekwipunek)
+'uzyj' [przedmiot]
+'sprawdz' [element otoczenia]
+'zaloz' [przedmiot]
+'atakuj' [cel]
+'bron' (sprawdź jaką trzymasz broń)
+'otoczenie' lub 'env' (ponownie sprawdź otoczenie)
+```"""
     await message.channel.send(text)
     return
